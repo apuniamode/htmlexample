3c0a277b91f5fd9ec010f47538e81aaa65d5884f
Amit <infopunia@gmail.com>
2019-07-16 03:18:38 +0530

    this is my first command

---
+++ js/adduser.js
@@ -0,0 +1,31 @@
+        $("#b1").click(function () {
+            $("#1").hide();
+            $("#2").show();
+
+        })
+        $("#b2").click(function () {
+            $("#2").hide();
+            $("#1").show();
+
+        })
+
+        $("#b3").click(function () {
+            $("#2").hide();
+            $("#3").show();
+
+        })
+
+        $("#b4").click(function () {
+            $("#3").hide();
+            $("#2").show();
+
+        })
+
+        $("#b1").click(function () {
+            $("#1").hide();
+            $("#2").show();
+
+        }) 
+
+        //Chat JS
+        
